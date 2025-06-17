3a317051794da839c4b7e35561852e124787ff43
Rashed Suwaidi <x.suwaidi@gmail.com>
2025-06-17 10:15:51 +0200

    Remove events from sidebar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,8 +1,5 @@
-import { getEvents } from '@/data'
 import { ApplicationLayout } from './application-layout'
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  let events = await getEvents()
-
-  return <ApplicationLayout events={events}>{children}</ApplicationLayout>
+  return <ApplicationLayout>{children}</ApplicationLayout>
 }
