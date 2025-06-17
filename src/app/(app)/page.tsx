25da3dd2fbbae72b4776233ebbb765367f7896c3
Rashed Suwaidi <x.suwaidi@gmail.com>
2025-06-17 10:05:14 +0200

    Remove tiptap editor

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,11 +1,5 @@
 "use client"
 
-import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor"
-
 export default function Home() {
-  return (
-    <>
-      <SimpleEditor />
-    </>
-  )
+  return <p>Welcome to Lire</p>
 }
